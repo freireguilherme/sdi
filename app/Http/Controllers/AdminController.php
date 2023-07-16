cbd1dac2fc357dd96fd91758c12cbd632cf75c97
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-07-16 17:00:40 -0300

    add: login and logout handlers controller

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    public function loginHandler(Request $request)
+    {
+        $fieldType = filter_var($request->login_id, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+
+        if($fieldType == 'email'){
+            $request->validate([
+                'login_id' => 'required|email|exists:admins,email',
+                'password' => 'required|min:5|max:45',
+            ],[
+                'login_id.required' => 'Email ou Username é obrigatório',
+                'login_id.email' => 'Email inválido',
+                'login_id.exists' => 'Email não existe no sistema',
+                'password.required' => 'A senha é obrigatória',
+            ]);
+        }else{
+            $request->validate([
+                'login_id' => 'required|exists:admins,username',
+                'password' => 'required|min:5|max:45',
+            ],[
+                'login_id.required' => 'Email ou Username é obrigatório',
+                'login_id.exists' => 'Username não existe no sistema',
+                'password.required' => 'A senha é obrigatória',
+            ]);
+        }
+
+        $creds = array(
+            $fieldType => $request->login_id,
+            'password' => $request->password
+        );
+
+        if(Auth::guard('admin')->attempt($creds)){
+            return redirect()->route('admin.home');
+        }else{
+            session()->flash('fail', 'Credenciais incorretas');
+            return redirect()->route('admin.login');
+        }
+    }
+
+    public function logoutHandler(Request $request)
+    {
+        Auth::guard('admin')->logout();
+        session()->flash('fail', 'Você saiu!');
+        return redirect()->route('admin.login');
+    }
+}
