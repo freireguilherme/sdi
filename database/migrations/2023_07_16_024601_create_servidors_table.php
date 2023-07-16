f090216d10564e39c512b60232f78245c72f6de8
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-07-16 04:08:00 -0300

    add: Admin and Servidor migrations

--- database/migrations/2023_07_16_024601_create_servidors_table.php
+++ database/migrations/2023_07_16_024601_create_servidors_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('servidors', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->string('password')->unique();
             $table->timestamps();
         });
     }
