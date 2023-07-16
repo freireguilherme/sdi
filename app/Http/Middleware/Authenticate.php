4c3e67ba9e8335663f5aa4d96fc9e9af10398c6f
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-07-16 16:59:21 -0300

    add: middleware redirects base on admin guard

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,12 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        //return $request->expectsJson() ? null : route('login');
+        if(!$request->expectsJson()){
+            if($request->routeIs('admin.*')){
+                session()->flash('fail', 'Você precisa entrar com sua conta primeiro');
+                return route('admin.login');
+            }
+        }
     }
 }
