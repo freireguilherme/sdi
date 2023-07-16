03886e311fd4f4df8aa2071bfb4a33f8d5889254
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-07-16 04:14:33 -0300

    add: cadidatos migration file, following his Model. on passwordd reset tokens table, add 'guard' collumn

--- database/migrations/2023_07_16_024804_create_candidatos_table.php
+++ database/migrations/2023_07_16_024804_create_candidatos_table.php
@@ -13,6 +13,22 @@ return new class extends Migration
     {
         Schema::create('candidatos', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('username')->nullable();
+            $table->string('email')->unique();
+            $table->string('password')->unique();
+            $table->string('endereço');
+            $table->string('telefone');
+            $table->string('sexo');
+            $table->string('nome_da_mae');
+            $table->date('data_nascimento');
+            $table->string('cpf');
+            $table->string('rg');
+            $table->string('rg_orgao_expedidor');
+            $table->date('rg_data_emissao');
+            $table->string('escolaridade');
+            $table->string('vinculo');
+            $table->timestamp('email_verifield_at')->nullable();
             $table->timestamps();
         });
     }
