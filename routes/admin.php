7e617a3f480ab8ee7c055f43d1ef40545ffdb27f
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-07-17 21:26:24 -0300

    add: create edital route

--- routes/admin.php
+++ routes/admin.php
@@ -13,5 +13,6 @@ Route::prefix('admin')->name('admin.')->group(function(){
     Route::middleware(['auth:admin', 'PreventBackHistory'])->group(function () {
         Route::view('/home', 'back.pages.admin.home')->name('home');
         Route::post('/logout_handler',[AdminController::class, 'logoutHandler'])->name('logout_handler');
+        Route::get('/create-edital',[AdminController::class, 'createEdital'])->name('create-edital');
     });
 });
