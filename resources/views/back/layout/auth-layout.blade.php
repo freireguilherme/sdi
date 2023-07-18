ca179e125c152489b7ca412705b1bd252174057f
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-07-17 21:11:39 -0300

    add: auth and pages layouts

--- resources/views/back/layout/auth-layout.blade.php
+++ resources/views/back/layout/auth-layout.blade.php
@@ -60,7 +60,9 @@
 				</div>
 				<div class="login-menu">
 					<ul>
-						<li><a href="register.html">Register</a></li>
+                        @if (!Route::is('admin.*'))
+						    <li><a href="register.html">Register</a></li>
+                        @endif
 					</ul>
 				</div>
 			</div>
@@ -85,6 +87,14 @@
 		<script src="/back/vendors/scripts/script.min.js"></script>
 		<script src="/back/vendors/scripts/process.js"></script>
 		<script src="/back/vendors/scripts/layout-settings.js"></script>
+        <script>
+            if( navigator.userAgent.indexOf("Firefox") != -1 ){
+                history.pushState(null, null, document.URL);
+                window.addEventListener('popstate' function(){
+                    history.pushState(null, null, document.URL);
+                });
+            }
+        </script>
         @stack('scripts')
 	</body>
 </html>
