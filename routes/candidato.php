d66452e6ccd8b286bf2393cbcf271dee34025924
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-07-19 03:50:33 -0300

    add: cadidate routes: login, login handler, homepage and resource(crud). Not finished yet

--- routes/candidato.php
+++ routes/candidato.php
@@ -1,7 +1,32 @@
 <?php
-
+/*
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CandidatoController;
 
-Route::get('/candidato', function(){
-    return 'hello candidato';
-});
+    //rotas de candidato sem necesidade de autorização
+    Route::prefix('candidatos')->group(function(){
+        Route::middleware(['guest:candidato', 'PreventBackHistory'])->group(function () {
+            Route::view('/login', 'back.pages.candidate.auth.login')->name('candidatos.login');
+            Route::post('/login_handler', [CandidatoController::class, 'loginHandler'])->name('candidatos.login_handler');
+            Route::resource('candidatos', CandidatoController::class)->only([
+                'create'
+            ]);
+        });
+
+        //rotas de candidato com autorização
+        Route::middleware(['auth:candidato', 'PreventBackHistory'])->group(function () {
+            Route::get('/', CandidatoController::class, 'inicio')->name('home');
+            Route::resource('candidatos', CandidatoController::class)->except([
+                'destroy'
+            ]);
+        });
+    });
+
+    */
+    use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\CandidatoController;
+
+    Route::view('/login', 'back.pages.candidate.auth.login')->name('candidatos.login');
+    Route::post('/login_handler', [CandidatoController::class, 'loginHandler'])->name('candidatos.login_handler');
+    Route::get('/', CandidatoController::class, 'inicio')->name('candidatos.home');
+    Route::resource('candidatos', CandidatoController::class);
