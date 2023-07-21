abcafd542a4fe133d8a5ddaeab1309157a241af7
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-07-21 14:11:00 -0300

    add: correct values and names for candidato creation form and submit button. Todo: verifications and date format

--- resources/views/back/pages/candidate/create.blade.php
+++ resources/views/back/pages/candidate/create.blade.php
@@ -5,88 +5,121 @@
         @csrf
         <div class="pd-20 card-box mb-30">
                 <div class="form-group row">
-                    <label class="col-sm-12 col-md-2 col-form-label">Text</label>
+                    <label class="col-sm-12 col-md-2 col-form-label">Nome</label>
                     <div class="col-sm-12 col-md-10">
-                        <input class="form-control" type="text" placeholder="Johnny Brown">
+                        <input class="form-control" type="text" placeholder="Seu nome completo" name="name">
                     </div>
                 </div>
                 <div class="form-group row">
                     <label class="col-sm-12 col-md-2 col-form-label">Email</label>
                     <div class="col-sm-12 col-md-10">
-                        <input class="form-control" value="bootstrap@example.com" type="email">
+                        <input class="form-control" value="exemplo@exemplo.com" type="email" name="email">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-sm-12 col-md-2 col-form-label">URL</label>
+                    <!-- aqui, ao inves de sexo, gênero. Genero é como a pessoa se vê -->
+                    <label class="col-sm-12 col-md-2 col-form-label">Qual o seu gênero</label>
                     <div class="col-sm-12 col-md-10">
-                        <input class="form-control" value="https://getbootstrap.com" type="url">
+                        <select class="form-control" id="exampleFormControlSelect1" name="sexo">
+                            <option>Femino</option>
+                            <option>Masculino</option>
+                            <option>Outro(Qual?)</option>
+                            <option>Prefiro não dizer</option>
+                        </select>
+                        <!-- caso Outro, habilitar -->
+                        <input class="form-control" type="text" name="sexo" disabled>
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-sm-12 col-md-2 col-form-label">Telephone</label>
+                    <label class="col-sm-12 col-md-2 col-form-label">Telefone</label>
                     <div class="col-sm-12 col-md-10">
-                        <input class="form-control" value="1-(111)-111-1111" type="tel">
+                        <input class="form-control" value="" type="tel" name="telefone">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-sm-12 col-md-2 col-form-label">Password</label>
+                    <label class="col-sm-12 col-md-2 col-form-label">Nome da mãe</label>
                     <div class="col-sm-12 col-md-10">
-                        <input class="form-control" value="password" type="password">
+                        <input class="form-control"  type="text" name="nome_da_mae">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-sm-12 col-md-2 col-form-label">Number</label>
+                    <label for="example-datetime-local-input" class="col-sm-12 col-md-2 col-form-label">Data de nascimento</label>
                     <div class="col-sm-12 col-md-10">
-                        <input class="form-control" value="100" type="number">
+                        <input class="form-control date-picker" placeholder="Escolha uma data ou digite" type="text" name="data_nascimento">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="example-datetime-local-input" class="col-sm-12 col-md-2 col-form-label">Date and time</label>
+                    <label class="col-sm-12 col-md-2 col-form-label">Número do CPF</label>
                     <div class="col-sm-12 col-md-10">
-                        <input class="form-control datetimepicker" placeholder="Choose Date anf time" type="text">
+                        <input class="form-control"  type="text" name="cpf">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-sm-12 col-md-2 col-form-label">Date</label>
+                    <label class="col-sm-12 col-md-2 col-form-label">Número do RG</label>
                     <div class="col-sm-12 col-md-10">
-                        <input class="form-control date-picker" placeholder="Select Date" type="text">
+                        <input class="form-control"  type="text" name="rg">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-sm-12 col-md-2 col-form-label">Month</label>
+                    <label class="col-sm-12 col-md-2 col-form-label">Órgão expedidor</label>
                     <div class="col-sm-12 col-md-10">
-                        <input class="form-control month-picker" placeholder="Select Month" type="text">
+                        <input class="form-control"  type="text" name="rg_orgao_expedidor">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-sm-12 col-md-2 col-form-label">Time</label>
+                    <label class="col-sm-12 col-md-2 col-form-label">Data de emissão</label>
                     <div class="col-sm-12 col-md-10">
-                        <input class="form-control time-picker td-input" placeholder="Select time" type="text" readonly="">
+                        <input class="form-control date-picker" placeholder="Escolha uma data ou digite" type="text" name="rg_data_emissao">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-sm-12 col-md-2 col-form-label">Select</label>
+                    <label class="col-sm-12 col-md-2 col-form-label">Endereço</label>
                     <div class="col-sm-12 col-md-10">
-                        <select class="custom-select col-12">
-                            <option selected="">Choose...</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
+                        <input class="form-control"  type="text" name="endereço">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-12 col-md-2 col-form-label">Escolaridade</label>
+                    <div class="col-sm-12 col-md-10">
+                        <select class="form-control" id="exampleFormControlSelect1" name="escolaridade">
+                            <option>Ensino Médio Completo</option>
+                            <option>Ensino Superior Inconpleto</option>
+                            <option>Ensino Superior Completo</option>
+                            <option>Mestre</option>
+                            <option>Doutor</option>
                         </select>
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-sm-12 col-md-2 col-form-label">Color</label>
+                    <label class="col-sm-12 col-md-2 col-form-label">Vínculo</label>
                     <div class="col-sm-12 col-md-10">
-                        <input class="form-control" value="#563d7c" type="color">
+                        <input class="form-control"  type="text" name="vinculo">
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label class="col-sm-12 col-md-2 col-form-label">Senha</label>
+                    <div class="col-sm-12 col-md-10">
+                        <input class="form-control" value="Sua senha" type="password" name="password">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-sm-12 col-md-2 col-form-label">Input Range</label>
+                    <label class="col-sm-12 col-md-2 col-form-label">Confirme a senha</label>
                     <div class="col-sm-12 col-md-10">
-                        <input class="form-control" value="50" type="range">
+                        <input class="form-control" value="Sua senha" type="password" name="password_confirm">
                     </div>
                 </div>
+
+        </div>
+        <div class="actions clearfix" style="padding: 10px">
+            <ul role="menu" aria-label="Pagination">
+                <button class="btn btn-primary" type="submit">
+                    Cadastrar
+                </button>
+                <button type="button" class="btn btn-danger">
+                    Cancelar
+                </button>
+            </ul>
         </div>
     </form>
 @endsection
