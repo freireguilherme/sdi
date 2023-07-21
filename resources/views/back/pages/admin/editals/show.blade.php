9f5b52d74e4aa5bf3f746059d77ef36a279a87cc
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-07-20 22:24:22 -0300

    add: Edit and Save buttons. Update route

--- resources/views/back/pages/admin/editals/show.blade.php
+++ resources/views/back/pages/admin/editals/show.blade.php
@@ -1,7 +1,8 @@
 @extends('back.layout.pages-layout')
 @section('pageTitle', isset($pageTitle) ? $pageTitle : 'Editar Edital')
 @section('content')
-    <form>
+    <form action="{{ route('editals.update', $edital->id) }}" method="POST">
+        @method('PUT')
         @csrf
         <div class="form-row">
             <div class="col">
@@ -9,31 +10,27 @@
                 <input class="form-control" type="text" placeholder="Entre com o nome do edital" value="{{ $edital->name}}" readonly="">
             </div>
             <div class="col">
-                <div class="form-group">
+                <div class="col-8 col-sm-4">
                     <label>Numero do edital</label>
                     <input class="form-control"  type="text" placeholder="Número do edital" value="{{ $edital->numero}}" readonly="">
                 </div>
-
             </div>
 
         </div>
         <div class="row">
-            <div class="col-sm-9">
+            <div class="col" style="margin-top: 15px">
                 <label>Período de Inscrição</label>
-                <div class="row">
-                    <div cclass="col-8 col-sm-6">
+                    <div class="col-8 col-sm-3">
                         <label>Data de início</label>
-                        <input class="form-control date-picker" placeholder="Select Date" type="text" name="data_inicio" value="{{ $edital->data_inicio}}" readonly="">
-                    </div>
-                    <div class="col-8 col-sm-6">
+                        <input class="form-control date-picker" type="text" id="data_inicio" value="{{ $edital->data_inicio}}" readonly="">
+
                         <label>Data final</label>
-                        <input class="form-control date-picker" placeholder="Select Date" type="text" name="data_fim" value="{{ $edital->data_fim}}" readonly="">
+                        <input class="form-control date-picker" type="text" name="data_fim" value="{{ $edital->data_fim}}" readonly="">
                     </div>
-                </div>
             </div>
         </div>
 
-        <div class="row" style="padding: 15px">
+        <div class="row" style="margin-top: 15px">
             <div class="col-md-4 col-sm-12">
                 <div class="form-group">
                     <label>Documentos a serem anexados</label>
@@ -79,14 +76,50 @@
                 <a class="btn btn-primary" href="{{route( 'admin.manage-editais' ) }}" role="button">Voltar</a>
             </div>
             <div class="col">
+                <button type="button" class="btn btn-warning" onclick="readOnlyOff()">Editar</button>
+                <button class="btn btn-primary" type="submit" disabled="" id="salvar">
+                    Salvar
+                </button>
+            </div>
+            <div class="col">
                 <a class="btn btn-primary" href="{{ route ('admin.edital-subs')}}" role="button">Acessar participantes incritos</a>
             </div>
         </div>
+
     </form>
+    @push('show-scripts')
         <script type="text/javascript">
             $('.form-control date-picker').datepicker({
                 format: "dd/mm/YYYY",
                 language: "pt-BR"
             });
         </script>
+        <script>
+            function readOnlyOff(){
+                var salvar = document.getElementById("salvar")
+                salvar.removeAttribute("disabled");
+
+                var inputs_elements = document.getElementsByTagName("input");
+                var inputs_elements2 = document.querySelectorAll('input[readonly]');
+                console.log(inputs_elements2);
+                for(let i in inputs_elements2){
+                    if (inputs_elements2[i].hasAttribute('readonly')){
+                        inputs_elements2[i].removeAttribute("readonly");
+                    }
+                    else
+                    {
+
+                    }
+                    /*else
+                    {
+                        if(typeof inputs_elements[i] === 'object' && inputs_elements[i] != null && 'setAttribute' in inputs_elements[i])
+                        {
+                            inputs_elements[i].setAttribute("readonly", "readonly");
+                        }
+                    }*/
+
+                }
+            }
+        </script>
+    @endpush
 @endsection
